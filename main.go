1ad9bb39ee9197bd485674014a21ab3ad9d24246
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-05 23:28:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import "SkyfallenDeveloperCenter/core"
+import (
+	"SkyfallenDeveloperCenter/core"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var globalExecTime time.Time
 
 func main() {
 
+	globalExecTime = time.Now()
+	SetupCloseHandler()
 	// Call the real main function
 	core.Handle()
 
+}
+
+func SetupCloseHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		fmt.Println("\r- Stopping Skyfallen App Engine")
+		elapsed := time.Since(globalExecTime).Minutes()
+		fmt.Printf(fmt.Sprintf("Stop complete, uptime is %v minutes.", elapsed))
+		os.Exit(0)
+	}()
 }
